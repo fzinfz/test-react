e906f3416f68719cba6bd7135059b08ca699750b
Ferro <fzinfz@gmail.com>
2023-03-12 23:52:05 +0800

    tmp update

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ https://reactrouter.com/en/main/start/tutorial
 
     npm create vite@latest name-of-your-project -- --template react
     npm install react-router-dom localforage match-sorter sort-by
+
+test until: https://reactrouter.com/en/main/start/tutorial#client-side-routing
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import Root, { loader as rootLoader } from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
